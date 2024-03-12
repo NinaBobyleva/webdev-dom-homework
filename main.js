c2c8ecbd168249c0678c81ca7537d59bbb299c5b
Nina Bobyleva <2847171@mail.ru>
2024-03-12 13:40:20 +0300

    Changed the files likes.js and main.js

--- main.js
+++ main.js
@@ -1,60 +1,61 @@
 import { getTodos, postTodo } from "./api.js";
+import { addLike } from "./likes.js";
 
 const nameInputElement = document.getElementById('name-input');
-  const textInputElement = document.getElementById('text-input');
-  const buttonElement = document.getElementById('add-button');
-  const deleteButtonElement = document.getElementById('delete-button');
-  const listElement = document.getElementById('list');
-  const loadElement = document.getElementById('load');
-  const loadCommentElement = document.getElementById('load-comment');
-  const formElement = document.getElementById('form');
+const textInputElement = document.getElementById('text-input');
+const buttonElement = document.getElementById('add-button');
+// const deleteButtonElement = document.getElementById('delete-button');
+const listElement = document.getElementById('list');
+const loadElement = document.getElementById('load');
+const loadCommentElement = document.getElementById('load-comment');
+const formElement = document.getElementById('form');
 
-  loadElement.classList.add('show');
+loadElement.classList.add('show');
 
-  const fetchAddRenderComments = () => {
+const fetchAddRenderComments = () => {
 
-    getTodos().then((responseData) => {
-        const appComments = responseData.comments.map((comment) => {
+getTodos().then((responseData) => {
+    const appComments = responseData.comments.map((comment) => {
 
-          return {
-            name: comment.author.name,
-            date: new Date(comment.date).toLocaleDateString('ru-RU') + " " + new Date(comment.date).toLocaleTimeString('ru-RU'),
-            text: comment.text,
-            likes: comment.likes,
-            isLiked: false,
-          };
-        });
-        loadElement.classList.remove('show');
-        comments = appComments;
-        renderComments();
-      });
-  }
+        return {
+        name: comment.author.name,
+        date: new Date(comment.date).toLocaleDateString('ru-RU') + " " + new Date(comment.date).toLocaleTimeString('ru-RU'),
+        text: comment.text,
+        likes: comment.likes,
+        isLiked: false,
+        };
+    });
+    loadElement.classList.remove('show');
+    comments = appComments;
+    renderComments();
+    });
+}
 
-  fetchAddRenderComments();
+fetchAddRenderComments();
 
-  let comments = [];
+let comments = [];
 
-  const renderComments = () => {
+const renderComments = () => {
     const commentsHtml = comments.map((comment, index) => {
-      return `<li data-text="${comment.text}\n${comment.name}" class="comment">
-          <div class="comment-header">
+        return `<li data-text="${comment.text}\n${comment.name}" class="comment">
+            <div class="comment-header">
             <div>${comment.name}</div>
             <div>${comment.date}</div>
-          </div>
-          <div class="comment-body">
-            <div class="comment-text">
-              ${comment.text.replaceAll('QUOTE_BEGIN', '<div class="quote">').replaceAll('QUOTE_END', '</div class="quote">')}
             </div>
-          </div>
-          <div class="comment-footer">
+            <div class="comment-body">
+            <div class="comment-text">
+                ${comment.text.replaceAll('QUOTE_BEGIN', '<div class="quote">').replaceAll('QUOTE_END', '</div class="quote">')}
+            </div>
+            </div>
+            <div class="comment-footer">
             <div class="edit">
-              <button class="edit-button">Редактировать</button>
+                <button class="edit-button">Редактировать</button>
             </div>
             <div class="likes">
-              <span class="likes-counter">${comment.likes}</span>
-              <button data-like="${comment.likes}" data-index="${index}" class="like-button ${comments[index].isLiked ? '-active-like' : 'like-button'}"></button>
+                <span class="likes-counter">${comment.likes}</span>
+                <button data-like="${comment.likes}" data-index="${index}" class="like-button ${comments[index].isLiked ? '-active-like' : 'like-button'}"></button>
             </div>
-          </div>
+            </div>
         </li>`;
     }).join('');
 
@@ -66,143 +67,128 @@ const nameInputElement = document.getElementById('name-input');
     initLikeButtonListeners();
     updateValue();
     answerComment();
-  };
+};
 
-  fetchAddRenderComments();
+fetchAddRenderComments();
 
 
-  function delay(interval = 300) {
+function delay(interval = 300) {
     return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve();
-      }, interval);
+        setTimeout(() => {
+            resolve();
+        }, interval);
     });
-  }
+}
 
 
-  const initLikeButtonListeners = () => {
-    const buttonElements = document.querySelectorAll('.like-button');
+const initLikeButtonListeners = () => {
+const buttonElements = document.querySelectorAll('.like-button');
 
     for (const buttonElement of buttonElements) {
 
-      const index = buttonElement.dataset.index;
-      const counter = buttonElement.dataset.like;
+        const index = buttonElement.dataset.index;
+        const counter = buttonElement.dataset.like;
 
-      buttonElement.addEventListener('click', (e) => {
-        e.stopPropagation();
-        buttonElement.classList.add('-loading-like');
+        buttonElement.addEventListener('click', (e) => {
+            e.stopPropagation();
+            buttonElement.classList.add('-loading-like');
 
-        delay(2000).then(() => {
-          
-          if (comments[index].isLiked === false) {
+            delay(2000).then(() => {
+                
+                addLike(comments, index, counter);
 
-            const result = +counter + 1;
-            comments[index].likes = result;
-            
-            comments[index].isLiked = true;
-          } else if (comments[index].isLiked === true) {
+                renderComments();
 
-            const result = counter - 1;
-            comments[index].likes = result;
-
-            comments[index].isLiked = false;
-          }
-
-          renderComments();
-
-        });
+            });
 
 
-      })
+        })
 
     }
 
-  }
+}
 
-  renderComments();
-
-
+renderComments();
 
 
-  function answerComment() {
-    const commentsElements = document.querySelectorAll('.comment');
+function answerComment() {
+const commentsElements = document.querySelectorAll('.comment');
 
     for (const comment of commentsElements) {
-      const text = comment.dataset.text;
+        const text = comment.dataset.text;
 
-      comment.addEventListener('click', () => {
+        comment.addEventListener('click', () => {
         sanitizeHtml(textInputElement.value = `QUOTE_BEGIN>${text}QUOTE_END`);
 
-      })
+        })
     }
-  }
+}
 
 
 
 
-  renderComments();
+renderComments();
 
 
-  textInputElement.addEventListener('keyup', keyEvent);
+textInputElement.addEventListener('keyup', keyEvent);
 
-  function keyEvent(e) {
+function keyEvent(e) {
     if (e.code === 'Enter') {
-      buttonElement.dispatchEvent(new Event('click'));
+        buttonElement.dispatchEvent(new Event('click'));
     }
+}
 
-  }
-
-  const sanitizeHtml = (htmlString) => {
+const sanitizeHtml = (htmlString) => {
     return htmlString.replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;');
-  }
+}
 
-  nameInputElement.addEventListener('input', validation);
-  textInputElement.addEventListener('input', validation);
+nameInputElement.addEventListener('input', validation);
+textInputElement.addEventListener('input', validation);
 
-  function validation() {
+function validation() {
     if (nameInputElement.value.trim() !== '') {
-      nameInputElement.classList.remove('error');
+        nameInputElement.classList.remove('error');
     }
     if (textInputElement.value.trim() !== '') {
-      textInputElement.classList.remove('error');
+        textInputElement.classList.remove('error');
     }
-  }
+}
 
 
 
-  buttonElement.disabled = true;
+buttonElement.disabled = true;
 
-  nameInputElement.addEventListener('input', updateValue);
-  textInputElement.addEventListener('input', updateValue);
+nameInputElement.addEventListener('input', updateValue);
+textInputElement.addEventListener('input', updateValue);
 
-  function updateValue() {
+function updateValue() {
     if (nameInputElement.value !== '' && textInputElement.value !== '') {
-      return buttonElement.disabled = false;
+        return buttonElement.disabled = false;
     } else {
-      return buttonElement.disabled = true;
+        return buttonElement.disabled = true;
     }
-  }
+}
 
-  renderComments();
+renderComments();
 
 
-  buttonElement.addEventListener('click', () => {
+buttonElement.addEventListener('click', () => {
 
     nameInputElement.classList.remove('error');
     textInputElement.classList.remove('error');
     if (nameInputElement.value.trim() !== '' && textInputElement.value.trim() === '') {
-      textInputElement.classList.add('error');
-      return;
+        textInputElement.classList.add('error');
+        return;
     }
     else if (nameInputElement.value.trim() === '' && textInputElement.value.trim() === '') {
-      nameInputElement.classList.add('error');
-      textInputElement.classList.add('error');
-      return;
+        nameInputElement.classList.add('error');
+        textInputElement.classList.add('error');
+        return;
     }
 
     if (textInputElement.value.trim() !== '' && nameInputElement.value.trim() === '') {
-      nameInputElement.classList.add('error');
-      return;
+        nameInputElement.classList.add('error');
+        return;
     }
 
     let nameElement;
@@ -211,8 +197,8 @@ const nameInputElement = document.getElementById('name-input');
     textInputElement.addEventListener('input', saveData);
 
     function saveData() {
-      nameElement = nameInputElement.value;
-      textElement = textInputElement.value;
+        nameElement = nameInputElement.value;
+        textElement = textInputElement.value;
     }
     saveData();
 
@@ -220,38 +206,38 @@ const nameInputElement = document.getElementById('name-input');
     formElement.classList.add('hide');
 
     const addComments = () => {
-      postTodo({name: nameInputElement.value, text: textInputElement.value}).then((responseData) => {
-          return fetchAddRenderComments();
+        postTodo({name: nameInputElement.value, text: textInputElement.value}).then((responseData) => {
+            return fetchAddRenderComments();
         }).
         then((response) => {
-          loadCommentElement.classList.remove('show');
-          formElement.classList.remove('hide');
-          nameInputElement.value = '';
-          textInputElement.value = '';
-          return fetchAddRenderComments();
+            loadCommentElement.classList.remove('show');
+            formElement.classList.remove('hide');
+            nameInputElement.value = '';
+            textInputElement.value = '';
+            return fetchAddRenderComments();
         })
         .catch((error) => {
-          if (error.message === 'Сервер сломался') {
+            if (error.message === 'Сервер сломался') {
             console.warn(error);
             // alert('Сервер сломался, попробуй позже');
             return addComments();
-          }
-          if (error.message === 'Введено меньше трех символов') {
+            }
+            if (error.message === 'Введено меньше трех символов') {
             loadCommentElement.classList.remove('show');
             formElement.classList.remove('hide');
             console.warn(error);
             return alert('Имя и комментарий не должны быть короче 3 символов');
-          }
-          buttonElement.disabled = false;
-          loadCommentElement.classList.remove('show');
-          formElement.classList.remove('hide');
-          console.warn(error);
-          alert("Кажется, у вас сломался интернет, попробуйте позже");
+            }
+            buttonElement.disabled = false;
+            loadCommentElement.classList.remove('show');
+            formElement.classList.remove('hide');
+            console.warn(error);
+            alert("Кажется, у вас сломался интернет, попробуйте позже");
         })
     }
     addComments();
-  });
+});
 
-  renderComments();
+renderComments();
 
-  console.log("It works!");
+console.log("It works!");
